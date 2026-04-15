4a6d021a3c03a11cb338a6d07cd6dee70dcd1dc0
arcskein <arcskein@gmail.com>
2026-04-15 13:53:21 +0300

    .

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,21 @@
 import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import ru from './locales/ru.json'
-import type { ILocaleMessage, Locale } from './types'
+import type { Locale } from './types'
+
+type MessageSchema = typeof en
 
 export const availableLocales = ['en', 'ru'] as const satisfies readonly Locale[]
 
-export const i18n = createI18n<[ILocaleMessage], Locale>({
+export const i18n = createI18n<[MessageSchema], Locale>({
   legacy: false,
   locale: 'en',
   fallbackLocale: 'en',
   globalInjection: true,
   messages: {
-    en: en as ILocaleMessage,
-    ru: ru as ILocaleMessage,
+    en,
+    ru,
   },
 })
+
+;(i18n.global.locale as any)
